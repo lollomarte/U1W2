10c5a55df3681ee75f82ac29b8b467af3a2bb920
Lorenzo Martegiani <lorenzomartegiani@gmail.com>
2022-12-06 17:21:59 +0100

    Initial commit

---
+++ index.js
@@ -0,0 +1,23 @@
+let name = "Lorenzo";
+console.log (name);
+
+let numero1 = 12
+let numero2 = 20
+console.log(numero1 + numero2)
+
+let x = 12;
+console.log(x);
+
+var nome ="Lorenzo"
+let cognome ="Martegiani"
+console.log(cognome)
+
+let y = 12;
+let numeroquattro = 4
+console.log(y - numeroquattro)
+
+let name1 = "john"
+let name2 = "John"
+console.log(name1 === name2)
+
+console.log(name1 === name2.toLowerCase())
